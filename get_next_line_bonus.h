faea6e80f65b2be43493f9cac8a3c2c773dee533
vicalvez <vicalvez@42nice.fr>
2023-12-21 17:45:48 +0100

    gnl

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vicalvez <vicalvez@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/21 09:28:28 by vicalvez          #+#    #+#             */
+/*   Updated: 2023/12/21 15:12:53 by vicalvez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+int		str_len(char *s);
+char	*get_next_line(int fd);
+char	*ft_substr(char const *s, size_t start, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *s);
+
+#endif
